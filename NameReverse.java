c5722ee87273cd42aa6a4b62c2d100e5698a50a6
Krishnareddy1995 <120888437+Krishnareddy1995@users.noreply.github.com>
2023-09-15 19:21:10 +0530

    new name reverse script

---
+++ NameReverse.java
@@ -0,0 +1,18 @@
+package javaPack;
+
+import java.util.Scanner;
+
+public class NameReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ String s="krishnareddy";
+ char ch[]=s.toCharArray();
+ for(int i=ch.length-1;i>=0;i--) {
+	 
+	 System.out.print(ch[i]);
+ }
+
+	}
+
+}
---
+++ Practice2.java
@@ -0,0 +1,24 @@
+package firstpackkkk;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class Practice2 {
+	WebDriver d;
+	@BeforeMethod
+	public void setup() {
+		d=new FirefoxDriver();
+		
+	}
+	
+	
+	
+	@Test
+	
+	public void load() {
+		
+	
+	
+}
+}
